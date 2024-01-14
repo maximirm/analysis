d91cbec545dfab968ad0f4c98364d1ab36412e38
maxim.irmscher <maxim.irmscher@idealo.de>
2024-01-14 21:28:03 +0100

    change stuff

--- app/api/analysis_controller.py
+++ app/api/analysis_controller.py
@@ -1,18 +1,11 @@
-
 from fastapi import APIRouter
 
-
-from app.services.schemas import schemas
-
-
 from app.services import analysis_service
+from app.services.schemas import schemas
 
 router = APIRouter()
 
 
 @router.post("/analyze/question", response_model=schemas.QuestionAnalyzed)
 async def analyze_question(question: schemas.Question):
-
     return await analysis_service.analyze_question(question)
-
-
